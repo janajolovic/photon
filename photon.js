f0764292b394b8a31317439b50744093c8b78cf9
Jana Jolovic <janajolovic04@gmail.com>
2021-09-14 21:02:44 +0200

    final changes

--- photon.js
+++ photon.js
@@ -2,17 +2,20 @@ const auth = "563492ad6f91700001000001c2d679f3950744a2953613ffe7a0491b";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
-const more = document.querySelector(".more")
+const more = document.querySelector(".more");
 let searchValue;
 let page = 1;
 let fetchLink;
+let currentSearch;
 
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", e => {
     e.preventDefault();
+    currentSearch = searchValue;
     searchPhotos(searchValue);
 })
-more.addEventListener("clink", loadMore)
+more.addEventListener("click", loadMore);
+     
 function updateInput(e) {
     searchValue = e.target.value;
 }
@@ -44,14 +47,14 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-    fetchLink = "https://api.pexels.com/v1/curated?per_page=15"
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
     const data = await fetchApi(fetchLink);
     generatePictures(data); 
 }
 
 async function searchPhotos(query) {
     clear();
-    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&pre_page=15`;
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
     const data = await fetchApi(fetchLink);
     generatePictures(data);
 }
@@ -63,6 +66,13 @@ function clear() {
 
 async function loadMore() {
     page++;
+    if(currentSearch) {
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`
+    } else {
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+    }
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
 }
 
 curatedPhotos();
